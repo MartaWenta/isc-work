8ec563708d98e2f9acab036d78430fdd4f0a047f
User 01 <user01@unst.(none)>
2017-02-28 16:49:09 +0000

    control flow task 3

--- basics.py
+++ basics.py
@@ -1,7 +1,5 @@
-mylist = [23,"hi",2.4e-10]
-print mylist
-count = 0
-while count < 3: #while count is less than 3 
-          item = mylist[count] 
-          print item, mylist.index(item) #index -prints position on the list
-          count += 1 #without it -neverending with 0s
+x = "hello"
+if x == 1:
+   print x, "Is true"
+else:
+   print "is false"
