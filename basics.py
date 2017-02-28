869598387667dd5fd110245d0b16a0c5b0ae0c53
User 01 <user01@unst.(none)>
2017-02-28 16:05:50 +0000

    first

--- basics.py
+++ basics.py
@@ -1,8 +1,18 @@
+#variables
 b=4
 c=3
-a=(b**2+c**2)**0.5
+a=(b**2+c**2)**0.5 #equation
 print a 
 
-print type(a)
+print type(a) #print the type of variable
 print type(b)
 print type(c)
+
+a=int(a)
+a,b,c=str(a),str(b),str(c) #transform to string
+
+
+print a + " squared equals " + b + " squared " + c + " squared ." #print types
+
+
+
