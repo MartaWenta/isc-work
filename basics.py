143d1a39273ffe46c6506802de8a2ae2f660a202
User 01 <user01@unst.(none)>
2017-02-28 16:58:40 +0000

    control flow task 3

--- basics.py
+++ basics.py
@@ -1,5 +1,5 @@
-x = "hello"
-if x == 1:
-   print x, "Is true"
-else:
-   print "is false"
+mylist = [1, 2, 3, 4, 5]
+print mylist
+print mylist[1] #python counts from 0 and prints 2 as the result
+print mylist[1:4] #prints second to fifth item
+
