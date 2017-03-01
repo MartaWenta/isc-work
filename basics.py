ea8fcd7ddcc4a356fb094ecfe3fd6c7e614a9603
User 01 <user01@unst.(none)>
2017-03-01 12:17:26 +0000

    Input and output-task 1-finish

--- basics.py
+++ basics.py
@@ -1,11 +1,6 @@
-#A tuple is a sequence of immutable Python objects. Tuples are sequences, just like lists.
-
-mylist = [23, "hi", 2.4e-10]
-
-(first, middle, last) = mylist
-print first,middle,last
-(first, middle, last) = (middle, last, first)
-print first, middle, last
+with open ('weather.csv','r') as reader:
+    data = reader.read() #reads the contents of the file to variable data 
+    print data
 
     
 
