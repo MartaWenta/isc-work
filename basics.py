036f4f13996ce7374ea9faf62f166da090cc7058
User 01 <user01@unst.(none)>
2017-03-01 14:57:10 +0000

    strings ex1

--- basics.py
+++ basics.py
@@ -1,19 +1,8 @@
-with open ('weather.csv','r') as reader:
-    lines = reader.readline() #read the first line
-    rain=[]
-    for line in reader.readlines(): #reads lines from a file
-        r=line.strip().split(",")[-1] #extract the values from last column
-        r= float(r) #make it a float number
-        rain.append(r) #append to the rain list
-print rain 
+s = 'I love to write python'
+for i in s:
+    print i, 
 
-with open ('myrain2.txt','w') as writer: #save to file
-  writer.write(str(rain))  
-    for r in rain:                      
-       writer.write(str(r)+"\n")
-    
-
-with open ('myrain2.txt','w') as writer: #save to file
-  writer.write(str(rain)) #also works but not as a column of values
-
+print '\n',s[4]
+print '\n',s[-1]
+print '\n',len(s)
 
