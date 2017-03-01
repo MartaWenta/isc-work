eb70214b4c51f5ec2f2ef0d3accefdddb932d49c
User 01 <user01@unst.(none)>
2017-03-01 11:04:28 +0000

    task 3 listing-part1

--- basics.py
+++ basics.py
@@ -1,13 +1,10 @@
-one = range(1,11)
-print one
-one[0]=10
-print one
-one.append(11)
-print one
-one.append([12,13,14]) #wrong [10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, [12, 13, 14]]
+forward=[]
+backward=[]
+values=["a","b","c"]
+for i in values:
+    forward.append(i)
+    backward.insert(0,i)
 
-print one
-one.extend([12,13,14])#correct 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, [12, 13, 14], 12, 13, 14]
-
-print one
+print forward
+print backward
 
