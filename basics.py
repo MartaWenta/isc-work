a2e3510f75eba14bf69fc0f5a0c1ea53e5492381
User 01 <user01@unst.(none)>
2017-03-01 16:14:06 +0000

    functions-ex 1

--- basics.py
+++ basics.py
@@ -1,18 +1,11 @@
-s= 'I love to write python'
-for i in s:
-    print i, 
+def double_it(number):
+    return number*2 
 
-print '\n',s[4]
-print '\n',s[-1]
-print '\n',len(s)
-split_s= s.split() #splits the string into words
-print split_s 
- 
-for word in split_s:
-    if word.find('i') > -1:
-        print "I found 'i' in '{0}'".format(word)
+x = double_it(2)
+print x
 
+y=double_it(3.4)
+print y
 
-
-
+print double_it("marta")
 
