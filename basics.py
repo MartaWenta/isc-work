2920bb837fd49c4d0336520d443ff8a8bf04a3e9
User 01 <user01@unst.(none)>
2017-03-01 11:58:21 +0000

    Tuples-task 3-finish

--- basics.py
+++ basics.py
@@ -2,11 +2,10 @@
 
 mylist = [23, "hi", 2.4e-10]
 
-for i,count in enumerate(mylist): 
-    print type(i),type(count) #i and count are either float or integer or string from mylist
-
-for iin enumerate(mylist): 
-    print type(i) #i is a tuple!
+(first, middle, last) = mylist
+print first,middle,last
+(first, middle, last) = (middle, last, first)
+print first, middle, last
 
     
 
