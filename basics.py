18504230ca6aac5a8405160c3d566b338529ea78
User 01 <user01@unst.(none)>
2017-03-01 12:35:34 +0000

    input,output tast 3.2.

--- basics.py
+++ basics.py
@@ -1,8 +1,11 @@
 with open ('weather.csv','r') as reader:
     lines = reader.readline() #read the first line
     rain=[]
-    for line in reader.readlines(): #reads lines from all lines
-        print line  
+    for line in reader.readlines(): #reads lines from a file
+        r=line.strip().split(",")[-1] #extract the values from last column
+        r= float(r) #make it a float number
+        rain.append(r) #append to the rain list
+print rain 
 
     
 
