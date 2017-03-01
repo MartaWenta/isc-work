557cec25a48d4eba6867d847d17dcf47ed33a7b7
User 01 <user01@unst.(none)>
2017-03-01 16:26:23 +0000

    functions-ex 3

--- basics.py
+++ basics.py
@@ -1,6 +1,13 @@
 def calc_hypo(a,b):
-    hypo = (a**2 + b**2)**0.5
-    return hypo
+    if type(a) not in (int,float) or type(b) not in (int,float): #check!
+        print 'wrong argument'
+        return False
+    if a <= 0 or b <= 0:
+        print 'wrong argument'
+        return False
+    else: #we do not need else here, then hypo and return are indented as if
+        hypo = (a**2 + b**2)**0.5
+        return hypo
 
 print calc_hypo(3,4)
 
