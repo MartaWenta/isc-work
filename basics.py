b8664d85660e9ff6709a4ad30bef73a1e747c513
User 01 <user01@unst.(none)>
2017-03-01 15:08:43 +0000

    strings ex2

--- basics.py
+++ basics.py
@@ -1,8 +1,7 @@
 s = 'I love to write python'
-for i in s:
-    print i, 
+split_s= s.split() #splits the string into words
+print split_s 
 
-print '\n',s[4]
-print '\n',s[-1]
-print '\n',len(s)
-
+for word in split_s:
+    if word.find('i') > -1:
+        print 'I found "i" in:', word
