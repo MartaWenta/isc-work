062c6c212ae00914fb4de206de0e4d9d09d94495
User 01 <user01@unst.(none)>
2017-03-01 11:36:32 +0000

    Tuples-task 1

--- basics.py
+++ basics.py
@@ -1,8 +1,9 @@
-countries=["uk","usa","uk","uae"]
-dir(countries)
-countries.count("uk")
+#A tuple is a sequence of immutable Python objects. Tuples are sequences, just like lists.
 
+t=(1,) #colon!
+print t[-1]
 
-
-
-
+d=range(100,201)
+tup=tuple(d)
+print tup
+print tup[0], tup[-1]
