1f90826a828dfd799ab64729d51949e291992894
User 01 <user01@unst.(none)>
2017-03-01 15:21:23 +0000

    strings ex2-another version

--- basics.py
+++ basics.py
@@ -1,15 +1,16 @@
-something = 'completly different'
-#print dir(something)
+s= 'I love to write python'
+for i in s:
+    print i, 
 
-print something.count('t')
-
-print something.find('plete')
-
-print something.split('e')
-
-thing2=something.replace('different','silly')
-print thing2
-
+print '\n',s[4]
+print '\n',s[-1]
+print '\n',len(s)
+split_s= s.split() #splits the string into words
+print split_s 
+ 
+for word in split_s:
+    if word.find('i') > -1:
+        print "I found 'i' in '{0}'".format(word)
 
 
 
