cfbd4692528c0b434711d9b240f626f9ff517aeb
User 01 <user01@unst.(none)>
2017-03-01 11:54:05 +0000

    Tuples-task 2-finish

--- basics.py
+++ basics.py
@@ -2,8 +2,12 @@
 
 mylist = [23, "hi", 2.4e-10]
 
-for (i,count) in enumerate(mylist):
-    print count, i
+for i,count in enumerate(mylist): 
+    print type(i),type(count) #i and count are either float or integer or string from mylist
+
+for iin enumerate(mylist): 
+    print type(i) #i is a tuple!
+
     
 
 
