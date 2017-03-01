b4a3ccad7ba3f47573ef2641f547e01f7e40c0bd
User 01 <user01@unst.(none)>
2017-03-01 11:41:24 +0000

    Tuples-task 2.1

--- basics.py
+++ basics.py
@@ -1,9 +1,9 @@
 #A tuple is a sequence of immutable Python objects. Tuples are sequences, just like lists.
 
-t=(1,) #colon!
-print t[-1]
+mylist = [23, "hi", 2.4e-10]
+for i in mylist: #step throurg items in a list to print index(count) of each items in the list
+   print i, mylist.index(i)
 
-d=range(100,201)
-tup=tuple(d)
-print tup
-print tup[0], tup[-1]
+
+
+
