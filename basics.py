039304bf52bd871a5342ed23be4bdd2f3c8f1423
User 01 <user01@teaching-F074.(none)>
2017-03-02 13:17:04 +0000

    numpy ax 2

--- basics.py
+++ basics.py
@@ -1,11 +1,9 @@
 import numpy as np
-
-x = range(1,11)
-a1 = np.array(x,np.int32)
-a2 = np.array(x,np.float32)
-
-print a1,a1.dtype
-
-print a2,a2.dtype
+a1 = np.zeros((2,3,4))
+print a1
+a2 = np.ones((2,3,4))
+print a2
+a3 = np.arange(1,1000)
+print a3
 
 
