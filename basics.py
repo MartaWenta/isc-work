ac34cdf8d89cd5891f4ed59ae39aea105964567b
User 01 <user01@unst.(none)>
2017-03-02 11:38:35 +0000

    sets ex 3

--- basics.py
+++ basics.py
@@ -1,12 +1,8 @@
-band = (["mel","geri","victoria","mel","emma"])
-counts = {}
-
-for name in band:
-    if name not in counts:
-        counts[name] = 1
-    else:
-        counts[name] += 1
-
-for name in counts:    #in order to print all names not only the first one
-    print name, counts[name] #prints name and number of times it appears
+d = {"maggie": "uk", "ronnie": "usa"}
+print dir(d)
+print d.items()
+print d.keys()
+print d.values()
+print d.get("maggies","nothing")
+print d.get("marta","gdynia")
 
