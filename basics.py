c28a1b559943ecf615ea85a20c9906dae0374d99
User 01 <user01@teaching-F074.(none)>
2017-03-02 13:10:51 +0000

    sets ex 3

--- basics.py
+++ basics.py
@@ -1,8 +1,11 @@
-d = {"maggie": "uk", "ronnie": "usa"}
-print dir(d)
-print d.items()
-print d.keys()
-print d.values()
-print d.get("maggies","nothing")
-print d.get("marta","gdynia")
+import numpy as np
+
+x = range(1,11)
+a1 = np.array(x,np.int32)
+a2 = np.array(x,np.float32)
+
+print a1,a1.dtype
+
+print a2,a2.dtype
+
 
