302d36f2b398826cb22af396e35f18aea5ddf6e3
User 01 <user01@teaching-F074.(none)>
2017-03-02 14:11:28 +0000

    arrays 2

--- basics.py
+++ basics.py
@@ -1,9 +1,17 @@
 import numpy as np
-a1 = np.zeros((2,3,4))
-print a1
-a2 = np.ones((2,3,4))
-print a2
-a3 = np.arange(1,1000)
-print a3
 
+a = np.array([2, 3.2, 5.5, -6.4, -2.2, 2.4])
+print a
+print a[1]
+print a[1:4]
+
+b = np.array([[2, 3.2, 5.5, -6.4, -2.2, 2.4],
+             [1, 22, 4, 0.1, 5.3, -9],
+             [3, 1, 2.1, 21, 1.1, -2]]) #nawiasyyy!!!!
+
+print b
+print b[:,3]
+print b[1:4,0:4]
+print b[1:,2]
+print b[:]
 
