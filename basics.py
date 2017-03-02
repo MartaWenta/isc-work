3212e1820b146821665305651e050e240307a747
User 01 <user01@teaching-F074.(none)>
2017-03-02 14:58:18 +0000

    array calculations ex3

--- basics.py
+++ basics.py
@@ -8,4 +8,7 @@ condition = np.logical_or(a < 3, a >8)
 new_a = np.where(condition, a*5, a*-5)
 print new_a
 
-print 3< a< 8
+#when only need a<3 and a>8
+condition = np.logical_or(a < 3, a >8)
+new_a = np.where(condition)
+print new_a
