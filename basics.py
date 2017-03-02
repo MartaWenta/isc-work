65dbb77849c5e87b804d1238ba96e3b36861650c
User 01 <user01@teaching-F074.(none)>
2017-03-02 16:26:52 +0000

    matplotlib 2

--- basics.py
+++ basics.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
-times= range(7)
-co2conc= [250, 265, 272, 260, 300, 320, 389]
-plt.plot(times, co2conc, 'r')
+times = range(7)
+co2conc = [250, 265, 272, 260, 300, 320, 389]
+temp = [14.1, 15.5, 16.3, 18.1, 17.3, 19.1, 20.2] 
+plt.plot(times, co2conc, 'r',
+        times, temp, 'g--')
 plt.title('exercise 2')
 plt.xlabel('times')
 plt.ylabel('conc')
