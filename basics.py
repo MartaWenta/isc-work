6420969fde5d2a42c197d150b407b5cf81ee071c
User 01 <user01@unst.(none)>
2017-03-02 10:23:16 +0000

    scripts and libraries ex 4

--- basics.py
+++ basics.py
@@ -1,17 +1,9 @@
-def calc_hypo(a,b):
-    if type(a) not in (int,float) or type(b) not in (int,float): #check!
-        print 'wrong argument'
-        return False
-    if a <= 0 or b <= 0:
-        print 'wrong argument'
-        return False
-    else: #we do not need else here, then hypo and return are indented as if
-        hypo = (a**2 + b**2)**0.5
-        return hypo
-
-print calc_hypo(3,4)
 
 
+import sys
+from dancing.dance import boogie
+moves = sys.argv[1:] #set a variable moves to all command line arguments sent to the script sys.argv
+boogie(moves)
 
 
 
